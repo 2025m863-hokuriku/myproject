6bfad62bd3c512f4ca9c83cba0b0b174ecc80773
2025m863-hokuriku <2025m863@hokuriku-u.ac.jp>
2026-01-22 21:11:46 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,3 +133,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ALLOWED_HOSTS = ['myproject-6gm9.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://myproject-6gm9.onrender.com']
